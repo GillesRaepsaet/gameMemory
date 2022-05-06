f132da710e9331933b2983ff0cc345b936093a8b
GillesRaepsaet <gillesr6000@hotmail.com>
2022-05-06 23:55:29 +0200

    gameMemoryAdd

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,43 @@
 const section = document.querySelector('section')
 const memoGame = document.getElementById('memoGame')
 
+
+//!image generator card
+
+const getDataImage = ()=>[
+    {imgSrc: "./img/vladimir.jpg", name: "vladimir"},
+    {imgSrc: "./img/vladimir.jpg", name: "vladimir"},
+    {imgSrc: "./img/aldebaran.jpg", name: "aledebaran"},
+    {imgSrc: "./img/aldebaran.jpg", name: "aledebaran"},
+    {imgSrc: "./img/c-dustin-K.jpg", name: "c-dustin.jpg"},
+    {imgSrc: "./img/c-dustin-K.jpg", name: "c-dustin.jpg"},
+    {imgSrc: "./img/greg-rakozy.jpg", name: "greg-rakozy"},
+    {imgSrc: "./img/greg-rakozy.jpg", name: "greg-rakozy"}, 
+    {imgSrc: "./img/diego-ph.jpg", name: "diego-ph"},
+    {imgSrc: "./img/diego-ph.jpg", name: "diego-ph"},
+    {imgSrc: "./img/jeremy-thomas.jpg", name:"jeremy-thomas"},
+    {imgSrc: "./img/jeremy-thomas.jpg", name:"jeremy-thomas"},
+    {imgSrc: "./img/jake-weirick.jpg", name:"jake-weirick"},
+    {imgSrc: "../img/kristopher-roller.jpg", name:"kristopher-roller"},
+    {imgSrc: "../img/kristopher-roller.jpg", name:"kristopher-roller"},
+    {imgSrc: "../img/guillermo-ferla.jpg", name:"guillermo-ferla"},
+    {imgSrc: "../img/mathias-p.jpg", name:"mathias-p"},
+    {imgSrc: "../img/mathias-p.jpg", name:"mathias-p"},
+    {imgSrc: "../img/nasa-Y.jpg", name:"nasa-Y"},
+    {imgSrc: "../img/nasa-Y.jpg", name:"nasa-Y"},
+    {imgSrc: "../img/dlxmedia-hu.jpg", name:"dlxmedia-hu"},
+    {imgSrc: "../img/dlxmedia-hu.jpg", name:"dlxmedia-hu"},
+    {imgSrc: "../img/nasa.jpg", name:"nasa.jpg"},
+    {imgSrc: "../img/nasa.jpg", name:"nasa"},
+    {imgSrc: "../img/pero-kalimero.jpg", name:"pero-kalimero"},
+    {imgSrc: "..//img/pero-kalimero.jpg", name:"pero-kalimero"},
+    {imgSrc: "../img/reign-abarintos.jpg" , name:"reign-abarintos"},
+    {imgSrc: "../img/reign-abarintos.jpg", name:"reign-abarintos"},
+    {imgSrc: "../img/sky.jpg", name:"sky"},
+    {imgSrc: "../img/sky.jpg", name:"sky"}
+]
+
+
+const imageData = getDataImage()
+
+console.log(getDataImage)
